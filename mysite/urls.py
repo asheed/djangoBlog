26a6f2188749c6f606a2d3e32456a82adf5515f5
Woojin Ahn <woojin7.ahn@samsung.com>
2017-01-12 21:51:41 +0900

    [Part1-2] url mapping for bookmark
    Change-Id: I614d4103281d8e2479c8c647d8c574ddbad04aca

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # Class-based views for Bookmark app
+    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+/$', BookmarkDV.as_view(), name='detail'),
 ]
