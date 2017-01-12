7316dabc6b54154d7ce5d8b6f9310d699cc7cb50
Woojin Ahn <woojin7.ahn@samsung.com>
2017-01-12 21:56:22 +0900

    [Part1-2] view 생성
    Change-Id: I87dfe9e5cb453f602beca46dd03ceacfaeae41b7

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+# view 생성
+#--- ListView
+class BookmarkLV(ListView):
+    model = Bookmark
+
+#--- DetailView
+class BookmarkDV(DetailView):
+    model = Bookmark
