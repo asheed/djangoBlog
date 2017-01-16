5081875151afd2c22e1abef531f28496b2fc5dba
Woojin Ahn <woojin7.ahn@samsung.com>
2017-01-16 22:35:33 +0900

    [Part1-3] 오타 수정
    Change-Id: I1a4abfea338ff8718596619850cdcb5e73a3c048

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^post/$', PostLV.as_view(), name='post_list'),
 
     # Example: /post/django-example/
-    url(r'^post/(?P<slug>[-\W]+)/$', PostDV.as_view(), name='post_detail'),
+    url(r'^post/(?P<slug>[-\w]+)/$', PostDV.as_view(), name='post_detail'),
 
     # Example: /archive/
     url(r'^archive/$', PostAV.as_view(), name='post_archive'),
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_month_archive'),
 
     # Example: /2012/nov/10/
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3}/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
 
     # Example: /today/
     url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
