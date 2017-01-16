3cd40855e36206d5391e3bfd42e612ebb89ea47d
Woojin Ahn <woojin7.ahn@samsung.com>
2017-01-16 22:37:01 +0900

    [Part1-3] admin 오타 수정
    Change-Id: I270118d0c9bdee01c62dcf823d6bbdc365719758

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from blog.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'modify_date')
-    list_filter = ('modify_date')
+    list_filter = ('modify_date',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
 
